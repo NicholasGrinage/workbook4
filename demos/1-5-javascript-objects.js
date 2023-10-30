7530b40f3a6283dea487f76f093fefed18552b43
Nicholas Grinage <ngrinage13@gmail.com>
2023-10-30 14:56:52 -0400

    1-5

---
+++ demos/1-5-javascript-objects.js
@@ -0,0 +1,11 @@
+// let employeedId = "1";
+// let name = "Ezra";
+// let jobTitle = "Theatre Teacher"
+// let payrate = 38.46;
+
+let employee = {
+  employeedId: "1",
+  name: "Ezra",
+  jobTitle: "Theatre Teacher",
+  payrate: 38.46,
+};
