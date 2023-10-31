6745572289f436e260a802f804dd21a7f2b79b32
Nicholas Grinage <ngrinage13@gmail.com>
2023-10-31 13:19:26 -0400

    array demo

---
+++ demos/2-4-while-loops.js
@@ -0,0 +1,6 @@
+let condition = true;
+let count = 1;
+while (count <= 10) {
+  console.log("doing it, ya know");
+  count = count + 1;
+}
---
+++ exercises/loopscripts/while-loops.js
@@ -0,0 +1,8 @@
+"use strict"
+let condition = true
+let count = 1
+while (count <=7) {
+    console.log("I love loops | while loops");
+    count++
+    
+}
